c9b8e6d57da6fa5ff68f96ddf433f1881d36c20d
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-11-21 00:35:15 +0530

    first commit

---
+++ configs/Connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Connection = async () => {
+  try {
+    await mongoose.connect(process.env.MongoURL);
+    console.log(`Database connected successfully`);
+  } catch (error) {
+    console.log(`failed to connect mongoDB ,error:${error}`);
+  }
+};
+
+module.exports = Connection;
